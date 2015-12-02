681cd2e3d90a5140cd7d8fb4268e12491782149d
gobo77 <gerald.siegert@outlook.com>
2015-12-02 22:58:50 +0100

    Teamhauptklassen hinzugefügt

---
+++ Simocracy Sports Simulator/Data/Footballteam.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simocracy.SportSim.Data
+{
+	/// <summary>
+	/// Fussballteam
+	/// </summary>
+	public class Footballteam : Team
+	{
+
+		#region Properties
+
+		/// <summary>
+		/// Gesamtstärke des Teams
+		/// </summary>
+		public override int Strength
+		{
+			get
+			{
+				return GoalkeeperStrength + DefenseStrength + MidfieldStrength + ForwardStrength;
+			}
+		}
+
+		/// <summary>
+		/// Anzahl der Spieler pro Team
+		/// </summary>
+		public override int PlayerCount
+		{
+			get
+			{
+				return 11;
+			}
+		}
+
+		/// <summary>
+		/// Stärke des Torhüters
+		/// </summary>
+		public int GoalkeeperStrength
+		{ get; set; }
+
+		/// <summary>
+		/// Stärke der Verteidigung
+		/// </summary>
+		public int DefenseStrength
+		{ get; set; }
+
+		/// <summary>
+		/// Stärke des Mittelfelds
+		/// </summary>
+		public int MidfieldStrength
+		{ get; set; }
+
+		/// <summary>
+		/// Stärke der Stürmer
+		/// </summary>
+		public int ForwardStrength
+		{ get; set; }
+
+		#endregion
+	}
+}
---
+++ Simocracy Sports Simulator/Data/Team.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Simocracy.SportSim.Data
+{
+	/// <summary>
+	/// Abstrakte Basisklasse für Teams
+	/// </summary>
+	public abstract class Team
+	{
+
+		#region Properties
+
+		/// <summary>
+		/// Name des Teams
+		/// </summary>
+		public string Name
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// Gesamtstärke des Teams
+		/// </summary>
+		public abstract int Strength
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Durchschnittsstärke eines Spielers
+		/// </summary>
+		public double StrengthPerPlayer
+		{
+			get
+			{
+				return Strength / 11;
+			}
+		}
+
+		/// <summary>
+		/// Anzahl der Spieler pro Team
+		/// </summary>
+		public abstract int PlayerCount
+		{ get; }
+
+		#endregion
+	}
+}
