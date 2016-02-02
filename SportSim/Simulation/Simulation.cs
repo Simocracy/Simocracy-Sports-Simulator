30a30f1ae4471a49b726024b3032044596627435
Laserflux <zimmer_t96@`web.de>
2016-02-02 20:03:01 +0100

    Neue Datei angelegt.

---
+++ SportSim/Simulation/Simulation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Simocracy.SportSim
+{
+    /// <summary>
+    /// Simulation
+    /// </summary>
+    
+    public class Simulation
+    {
+
+        var teamA, teamB;
+
+        public New(int idA, int idB)
+        {
+            teamA = Settings.FootballTeams.get(idA);
+            teamB = Settings.FootballTeams.get(idB);
+        }
+       
+    }
+}
