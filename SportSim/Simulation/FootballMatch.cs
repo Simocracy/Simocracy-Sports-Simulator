4c4ffdf306b9914037884b7af870fc10ccc3da38
gobo7793 <gerald.siegert@outlook.com>
2016-04-01 13:31:22 +0200

    Simulation in FootballMatch integriert

---
+++ SportSim/Simulation/FootballMatch.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simocracy.SportSim.Simulation
+{
+
+	/// <summary>
+	/// Fußballspiel
+	/// </summary>
+	public class FootballMatch
+	{
+		#region Simulation Members
+
+		private int _Ball;
+		private int _Minutes;
+		private Random _Rand = new Random();
+		private int _Start;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initiiert ein neues Fußballspiel
+		/// </summary>
+		/// <param name="idA">ID des Heimteams</param>
+		/// <param name="idB">ID des Auswärtsteams</param>
+		public FootballMatch(int idA, int idB)
+			: this(Settings.FootballTeams.Get(idA), Settings.FootballTeams.Get(idB))
+		{ }
+
+		/// <summary>
+		/// Initiiert ein neues Fußballspiel
+		/// </summary>
+		/// <param name="idA">ID des Heimteams</param>
+		/// <param name="idB">ID des Auswärtsteams</param>
+		/// <param name="minutes">Anzahl Spielminuten</param>
+		public FootballMatch(int idA, int idB, int minutes)
+			: this(Settings.FootballTeams.Get(idA), Settings.FootballTeams.Get(idB), minutes)
+		{ }
+
+		/// <summary>
+		/// Initiiert ein neues Fußballspiel
+		/// </summary>
+		/// <param name="teamA">Heimteam</param>
+		/// <param name="teamB">Auswärtsteam</param>
+		public FootballMatch(FootballTeam teamA, FootballTeam teamB)
+			: this(teamA, teamB, 90)
+		{ }
+
+		/// <summary>
+		/// Initiiert ein neues Fußballspiel
+		/// </summary>
+		/// <param name="teamA">Heimteam</param>
+		/// <param name="teamB">Auswärtsteam</param>
+		/// <param name="minutes">Anzahl Spielminuten</param>
+		public FootballMatch(FootballTeam teamA, FootballTeam teamB, int minutes)
+		{
+			TeamA = teamA;
+			TeamB = teamB;
+			_Minutes = minutes;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Heimteam
+		/// </summary>
+		public FootballTeam TeamA { get; private set; }
+
+		/// <summary>
+		/// Auswärtsteam
+		/// </summary>
+		public FootballTeam TeamB { get; private set; }
+
+		/// <summary>
+		/// Tore Heimteam
+		/// </summary>
+		public int ResultA { get; set; }
+
+		/// <summary>
+		/// Tore Auswärtsteam
+		/// </summary>
+		public int ResultB { get; set; }
+
+		#endregion
+
+		#region Simulation
+
+		/// <summary>
+		/// Simuliert ein Fußballspiel
+		/// </summary>
+		public void Simulate()
+		{
+			_Ball = Kickoff();
+			for(int i = 1; i <= _Minutes; i++)
+			{
+				if(i == 45)
+				{
+					if(_Ball == 14)
+						ResultA++;
+					if(_Ball == 24)
+						ResultB++;
+					_Ball = _Start;
+				}
+
+				switch(_Ball)
+				{
+					case 10:
+						_Ball = Turn(TeamA.GoalkeeperStrength, TeamB.ForwardStrength);
+						break;
+					case 11:
+						_Ball = Turn(TeamA.DefenseStrength, TeamB.MidfieldStrength);
+						break;
+					case 12:
+						_Ball = Turn(TeamA.MidfieldStrength, TeamB.DefenseStrength);
+						break;
+					case 13:
+						_Ball = Turn(TeamA.DefenseStrength, TeamB.GoalkeeperStrength + TeamB.DefenseStrength / 2);
+						break;
+					case 14:
+						ResultA++;
+						_Ball = 22;
+						break;
+					case 20:
+						_Ball = Turn(TeamB.GoalkeeperStrength, TeamA.ForwardStrength);
+						break;
+					case 21:
+						_Ball = Turn(TeamB.DefenseStrength, TeamA.MidfieldStrength);
+						break;
+					case 22:
+						_Ball = Turn(TeamB.MidfieldStrength, TeamA.DefenseStrength);
+						break;
+					case 23:
+						_Ball = Turn(TeamB.DefenseStrength, TeamA.GoalkeeperStrength + TeamA.DefenseStrength / 2);
+						break;
+					case 24:
+						ResultB++;
+						_Ball = 12;
+						break;
+				}
+			}
+		}
+
+		private int Turn(int strength1, int strength2)
+		{
+			int random = _Rand.Next(strength1 + strength2);
+			if(random < strength1)
+				_Ball++;
+			else
+				switch(_Ball)
+				{
+					case 10:
+						return 23;
+					case 11:
+						return 22;
+					case 12:
+						return 21;
+					case 13:
+						return 20;
+					case 20:
+						return 13;
+					case 21:
+						return 12;
+					case 22:
+						return 11;
+					case 23:
+						return 10;
+				}
+
+			return 0;
+		}
+
+		private int Kickoff()
+		{
+			int random = _Rand.Next(2);
+			if(random == 0)
+			{ _Start = 22; return 12; }
+			if(random == 1)
+			{ _Start = 12; return 22; }
+			else
+				return 0;
+		}
+
+		#endregion
+
+	}
+}
---
+++ SportSim/Simulation/LeagueSimulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simocracy.SportSim
+{
+	/// <summary>
+	/// Simulation einer Fußballliga bzw. Turniergruppe
+	/// </summary>
+	public class LeagueSimulation
+	{
+		#region Properties
+
+		/// <summary>
+		/// Teams der Liga
+		/// </summary>
+		FootballTeamCollection Teams { get; set; }
+
+		
+
+		#endregion
+
+		#region Constructor
+
+		#endregion
+
+		#region Simulation
+
+		#endregion
+	}
+}
