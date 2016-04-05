ebb5c6a5fd1cf7b39de4acb4a7b1286d265faf25
gobo7793 <gerald.siegert@outlook.com>
2016-04-05 03:17:23 +0200

    Übersetzungen für GUI

--- SportSim/Data/Enums.cs
+++ SportSim/Data/Enums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,26 +15,31 @@ namespace Simocracy.SportSim
 		/// <summary>
 		/// Generisches Stadion für Platzhalter
 		/// </summary>
+		[Description("Generisches Stadion")]
 		GenericStadium = 0,
 
 		/// <summary>
 		/// Unbekannter Stadiontyp
 		/// </summary>
+		[Description("Unbekanntes Stadion")]
 		UnknownStadium = 1,
 
 		/// <summary>
 		/// Reines Fussballstadion
 		/// </summary>
+		[Description("Reines Fußballstadion")]
 		FootballStadium = 2,
 
 		/// <summary>
 		/// Stadion mit Laufbahn und Leichtathletikanlagen
 		/// </summary>
+		[Description("Stadion mit Leichtathletikanlage")]
 		AthleticStadium = 3,
 
 		/// <summary>
 		/// Multifunktionsarena
 		/// </summary>
+		[Description("Multifunktionsarena")]
 		MultiPurposeStadium = 4
 	}
 
@@ -45,31 +51,37 @@ namespace Simocracy.SportSim
 		/// <summary>
 		/// Unbekannter Kontinent
 		/// </summary>
+		[Description("Unbekannt")]
 		Unknown = 0,
 
 		/// <summary>
 		/// Amerika
 		/// </summary>
+		[Description("Amerika")]
 		America = 1,
 
 		/// <summary>
 		/// Europa
 		/// </summary>
+		[Description("Europa")]
 		Europe = 2,
 
 		/// <summary>
 		/// Afrika
 		/// </summary>
+		[Description("Afrika")]
 		Africa = 3,
 
 		/// <summary>
 		/// Asien
 		/// </summary>
+		[Description("Asien")]
 		Asia = 4,
 
 		/// <summary>
 		/// Ozeanien
 		/// </summary>
+		[Description("Ozeanien")]
 		Oceania = 5
 	}
 
@@ -81,16 +93,19 @@ namespace Simocracy.SportSim
 		/// <summary>
 		/// Einfache Runde, nur Hinspiele
 		/// </summary>
+		[Description("Einzelrunde")]
 		SingleRound = 0,
 
 		/// <summary>
 		/// Doppelrunde, Hin- und Rückspiele
 		/// </summary>
+		[Description("Doppelrunde")]
 		DoubleRound = 1,
 
 		/// <summary>
 		/// Vierfachrunde, Je 2 Hin- und Rückspiele
 		/// </summary>
+		[Description("Vierfachrunde")]
 		QuadrupleRound = 2
 
 	}
