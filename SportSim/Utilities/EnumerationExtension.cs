1c46c08f092232bbc36d6b0a9b534bb2ca04cb3a
gobo7793 <gerald.siegert@outlook.com>
2016-04-06 00:21:04 +0200

    Umbenannt

--- SportSim/Utilities/EnumerationExtension.cs
+++ SportSim/Utilities/EnumerationExtension.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace Simocracy.SportSim
